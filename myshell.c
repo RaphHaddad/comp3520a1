657d0fab2e2a3979de66e26e5f17b9b3d4b41bd2
Haddad; Raphael <Haddad; Raphael rhad2518@ug.it.usyd.edu.au>
2012-04-23 21:22:43 +1000

    echo

--- myshell.c
+++ myshell.c
@@ -2,9 +2,14 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #define MAX_BUFFER 1024                        /* max line buffer */
 #define MAX_ARGS 64                            /* max # args */
 #define SEPARATORS " \t\n"                     /* token sparators */
+
+extern char **environ;
+
+
 int main (int argc, char ** argv)
 {
     char buf[MAX_BUFFER];                      /* line buffer */
@@ -31,8 +36,33 @@ int main (int argc, char ** argv)
                 if (!strcmp(args[0],"quit"))   /*  "quit" command */
                     break;                     /*  break out of 'while' loop */
                 if (!strcmp(args[0],"dir")){/*TODO: implment dir */
-					system("ls -al");
+                	if (args[1]) {/*if something after dir*/
+						char *ls = "ls -al ";
+						char *str;
+						str = malloc(strlen(ls) + strlen(args[1])+1);
+						strcat(str,ls);
+						strcat(str,args[1]);
+						system(str);
+						free(str);
+					} else {/* if no directory is selected assume it is current directory */
+						system("ls -al .");
+					}
 				}
+				if (!strcmp(args[0],"environ")){
+					char ** env = environ;
+					while (*env) printf("%s\n",*env++);
+				}
+				if (!strcmp(args[0],"echo")) {
+						int i= 1;
+						int lengthStr = 5;/* the word 'echo' is 4 characters with a space it is 5 */
+						while (args[i]){
+							lengthStr = strlen(args[i])+ lengthStr;
+							i = i + 1;
+						}
+						printf("%i",lengthStr);
+				}
+			
+			
 				/* else pass command onto OS (or in this instance, print them out) */
                 arg = args;
                 while (*arg) fprintf(stdout,"%s ",*arg++);
@@ -45,5 +75,5 @@ int main (int argc, char ** argv)
     }
 
     return 0; 
-
+/*asdf*/
 }
